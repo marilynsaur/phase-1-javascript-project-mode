a5928e7dc331275f1fe9b95e547e2f5864a59b76
Marilyn Wren <Marilynwren64@yahoo.com>
2022-04-18 16:54:36 -0400

    restructuring project

--- index.js
+++ index.js
@@ -1,117 +1,138 @@
-const submitForm = () => {
-    const inputForm = document.querySelector('form');
-  
-    inputForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const input = document.querySelector('p');
-  
-      console.log(input.value);
-  
-      fetch('http://localhost:3000/trains')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
 
-        data.forEach(train => createTrains(train))
-      })
+//This is where the trains loads up on the page
+document.addEventListener("DOMContentLoaded", () => {
+  //this function adding the trains and fetching them to the DOM
+  fetch('http://localhost:3000/trains')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
 
-  })
-}
-  document.addEventListener('DOMContentLoaded', submitForm);
-  
-
-  function createTrains(data) {
-    container = document.querySelector("#train-collection");
-    console.log(container)
-    let card = document.createElement('div');
-    card.innerHTML =
-    `<div class="card">
-    <h2 class= "title">${data.name}</h2>
-    <img src=${data.image} class="train-avatar" />
-    <div class="container">
-        <p class="description">${data.description}</p>
-        <ul class="list">
-          <li><span class="label">${data.list}</li>
-        </ul>
-        <p class="fact">${data.fact}</p>
-       
-        <button class="like-btn" id=${data.id}>Like ❤️</button>
-     </div>
-   `
-    container.appendChild(card);
-
-  
-  const totalCount = card.querySelector("p");
-  const incrementCount = card.querySelector('.like-btn');
-  console.log(incrementCount)
-  let count = data.likes;
- 
-
-   const handleIncrement = () => {
-     console.log(incrementCount)
-     count++;
-     totalCount.innerText =`${count} likes!`;
-     console.log(count)
-     
-
-   };
-  
-  incrementCount.addEventListener("click",handleIncrement);
-  }
-
-  const trainContainer = document.querySelector('#train-collection');
- 
-  function handleSubmit(e) {
-    e.preventDefault()
-
-    let trainObj = {
-      name: e.target.name.value,
-      image: e.target.image.value,
-      likes: 0
-    }
-
-    postTrains(trainObj);
-    
-
-    createTrains(trainObj);
-   
-  }
-  trainContainer.addEventListener('submit', handleSubmit);
-
-
-  function postTrains(trainObj) {
-    console.log(trainObj)
-    fetch('http://localhost:3000/trains', {
-      method: 'POST',
-      headers:
-      {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-
-      body: JSON.stringify(trainObj)
+      data.forEach(train => createTrains(train))
     })
-      .then(response => response.json())
-      .then(newTrain => createTrains(newTrain))
+  trainLoads();
+  //trainInput();
 
+})
+
+
+
+
+
+const trainLoads = () => {
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('p');
+
+    console.log(input.value);
+
+
+    function createTrains(data) {
+      container = document.querySelector("#train-collection");
+      console.log(container)
+      let card = document.createElement('div');
+      card.innerHTML =
+        `<div class="card">
+        <h2 class= "title">${data.name}</h2>
+        <img src=${data.image} class="train-avatar" />
+        <div class="container">
+            <p class="description">${data.description}</p>
+            <ul class="list">
+              <li><span class="label">${data.list}</li>
+            </ul>
+            <p class="fact">${data.fact}</p>
+           
+            <button class="like-btn" id=${data.id}>Like ❤️</button>
+         </div>
+       `
+      container.appendChild(card);
+
+      function patchTrains(train, count) {
+        const totalCount = card.querySelector("p");
+        const incrementCount = card.querySelector('.like-btn');
+        console.log(incrementCount)
+        let count = data.likes;
+
+
+        const handleIncrement = () => {
+          console.log(incrementCount)
+          count++;
+          totalCount.innerText = `${count} likes!`;
+          console.log(count)
+
+
+        };
+
+        incrementCount.addEventListener("click", handleIncrement);
+      }
+
+      fetch(`http://localhost:3000/trains/${train.id}`, {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+
+        body: JSON.stringify({ likes: count })
+
+      })
+        .then(response => response.json())
+        .then(likesTrain => createTrains(likesTrain))
+
+
+
+
+    }
+})
+
+
+
+
+
+
+
+
+const trainContainer = document.querySelector('#train-collection');
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  let trainObj = {
+    name: e.target.name.value,
+    image: e.target.image.value,
+    description: e.target.description.value,
+    likes: 0
   }
 
-  function patchTrains(train,count) {
-    console.log(toy)
-    fetch(`http://localhost:3000/trains/${train.id}`, {
-      method: 'PATCH',
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
+  postTrains(trainObj);
 
-      body: JSON.stringify({likes:count})
-      
+
+  createTrains(trainObj);
+
+}
+trainContainer.addEventListener('submit', handleSubmit);
+
+
+function postTrains(trainObj) {
+  console.log(trainObj)
+  fetch('http://localhost:3000/trains', {
+    method: 'POST',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+
+    body: JSON.stringify(trainObj)
   })
-  .then(response => response.json())
-  .then(likesTrain => createTrains(likesTrain))
+    .then(response => response.json())
+    .then(newTrain => createTrains(newTrain))
+
+}
+
+  
 
 }
 
 
-  
